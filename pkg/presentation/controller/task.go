760c2a9093ecc55c3483bc0b1591fe755b75dbb7
Hiroki Hayashi <66511809+hiroki90@users.noreply.github.com>
2021-03-20 23:32:35 +0900

    feat: task周りのmockAPIの追加 (#10)
    * feat: task取得のmockAPIの作成
    
    * feat: POST /tasks/doneのmockAPIの作成
    
    * fix: aPI仕様に合うようにkey名などを修正
    
    * fix: scopeや重複している構造体などの修正
    
    Co-authored-by: ari1021 <o.pe.3.vndt.ku@gmail.com>

--- pkg/presentation/controller/task.go
+++ pkg/presentation/controller/task.go
@@ -14,25 +14,95 @@ func NewTask() *Task {
 	return &Task{}
 }
 
-type CreateTaskRequest struct {
+type taskID struct {
+	ID string `json:"id"`
+}
+
+type task struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	IsDone      bool   `json:"isDone"`
+}
+
+type createTaskRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
 
 type mockCreateTaskResponse struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	task
+}
+
+type mockGetTaskResponse struct {
+	Tasks []*task `json:"tasks"`
+}
+
+type taskDoneRequest struct {
+	TaskIDs []*taskID `json:"taskIDs"`
+}
+
+type mockTaskDoneResponse struct {
+	Tasks []*task `json:"tasks"`
 }
 
 func (t *Task) HandleCreateTask(c echo.Context) error {
-	req := new(CreateTaskRequest)
+	req := new(createTaskRequest)
 	if err := c.Bind(req); err != nil {
 		log.Println(err)
 		return echo.NewHTTPError(http.StatusBadRequest, "invalid request body")
 	}
 	mockRes := &mockCreateTaskResponse{
-		Name:        req.Name,
-		Description: req.Description,
+		task{
+			ID:          "id",
+			Name:        req.Name,
+			Description: req.Description,
+			IsDone:      false,
+		},
+	}
+	return c.JSON(http.StatusOK, mockRes)
+}
+
+func (t *Task) HandleGetTask(c echo.Context) error {
+	var tasks []*task
+	tasks = append(tasks, &task{
+		ID:          "testID1",
+		Name:        "testName1",
+		Description: "testDescription1",
+		IsDone:      true,
+	})
+	tasks = append(tasks, &task{
+		ID:          "testID2",
+		Name:        "testName2",
+		Description: "testDescription2",
+		IsDone:      false,
+	})
+	mockRes := &mockGetTaskResponse{
+		Tasks: tasks,
+	}
+
+	return c.JSON(http.StatusOK, mockRes)
+}
+
+func (t *Task) HandleTaskDone(c echo.Context) error {
+	req := new(taskDoneRequest)
+	if err := c.Bind(req); err != nil {
+		log.Println(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid request body")
+	}
+	taskIDs := req.TaskIDs
+	tasks := make([]*task, 0, len(taskIDs))
+	for _, taskID := range taskIDs {
+		task := &task{
+			ID:          taskID.ID,
+			Name:        "doneName",
+			Description: "doneDescription",
+			IsDone:      true,
+		}
+		tasks = append(tasks, task)
+	}
+	mockRes := mockTaskDoneResponse{
+		Tasks: tasks,
 	}
 	return c.JSON(http.StatusOK, mockRes)
 }
--- pkg/presentation/router.go
+++ pkg/presentation/router.go
@@ -16,5 +16,7 @@ func NewEcho() *echo.Echo {
 
 	t := controller.NewTask()
 	e.POST("/tasks", t.HandleCreateTask)
+	e.GET("/tasks", t.HandleGetTask)
+	e.POST("/tasks/done", t.HandleTaskDone)
 	return e
 }
