a7772107ce52bd77f0af0ef6664ff0ed284b03ff
ari1021 <o.pe.3.vndt.ku@gmail.com>
2021-03-26 15:31:32 +0900

    feat: task entityを追加

---
+++ pkg/domain/entity/task.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"time"
+)
+
+type Task struct {
+	ID          string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   time.Time
+	Name        string
+	Description *string
+	IsDone      bool
+	UserID      string
+}
